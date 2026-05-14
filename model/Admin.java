a00da15df2bdd80404f64589545667fd7eff220e
FardowsAdam <ashaqfrdws@gmail.com>
2026-05-14 11:02:38 +0300

    testing changes

--- model/Admin.java
+++ model/Admin.java
@@ -12,7 +12,7 @@ public class Admin extends User {
 
     @Override
     public void login() {
-        System.out.println("Administrator " + username + " logged in With success.");
+        System.out.println("Administrator " + username + " logged in.");
     }
 
     @Override
